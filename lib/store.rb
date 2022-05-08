c97d4bf34fe99e27fc1a73c6d1a6f0ec6bc371bd
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-05-07 22:33:04 -0400

    ex6

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,6 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3, message: 'Store has to with a name and have a minimum of three characters.'}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'Store has a revenue > 0' }
+  end
 end
